315cf4ba1a90fe0428645a1613f9278d16d89ee3
Schlabonski <fabian.thielemann@uranus.uni-freiburg.de>
2022-04-26 19:13:08 +0200

    add led stripes to light show

--- light_show.ino
+++ light_show.ino
@@ -9,10 +9,11 @@
 #define AUDIO_IN_PIN    34            // Signal in on this pin
 
 #define NUM_BANDS       8            // To change this, you will need to change the bunch of if statements describing the mapping from bins to bands
-#define NOISE           300           // Used as a crude noise filter, values below this are ignored
+#define NOISE           1000           // Used as a crude noise filter, values below this are ignored
 
 #define ONBOARD_LED     2
 #define FLOODLIGHT_OUT 25
+#define LED_STRIPE_OUT_0 2
 
 // Sampling and FFT stuff
 unsigned int sampling_period_us;
@@ -22,13 +23,32 @@ int bandValues[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 double vReal[SAMPLES];
 double vImag[SAMPLES];
 unsigned long newTime;
-bool beat_detected = false;
+bool beat_detected_low = false;
+bool beat_detected_high = false;
 arduinoFFT FFT = arduinoFFT(vReal, vImag, SAMPLES, SAMPLING_FREQ);
 
+// led stripes
+const int freq = 5000;
+const int ledChannel_0 = 0;
+const int resolution = 8;
+byte brightness_stripe_0 = 0;
+
+// floodlights
+byte brightnness_floodlight = 0;
+int counter_low = 0;
+
+
 void setup() {
+  // FFT setting
   sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQ));
+
+  // onboard LED for debugging
   pinMode(ONBOARD_LED, OUTPUT);
   digitalWrite(ONBOARD_LED, HIGH);
+
+  // LED stripes
+  ledcSetup(ledChannel_0, freq, resolution);
+  ledcAttachPin(LED_STRIPE_OUT_0, ledChannel_0);
 
 }
 
@@ -57,8 +77,8 @@ void loop() {
     if (vReal[i] > NOISE) {                    // Add a crude noise filter
 
     //8 bands, 12kHz top band
-      if (i<=3 )           bandValues[0]  += (int)vReal[i];
-      if (i>3   && i<=6  ) bandValues[1]  += (int)vReal[i];
+      if (i > 0 && i<=6 )           bandValues[0]  += (int)vReal[i]; // volume band
+      if (i>3   && i<=6  ) bandValues[1]  += (int)vReal[i]; // messed with the value to combine lower, old was 3
       if (i>6   && i<=13 ) bandValues[2]  += (int)vReal[i];
       if (i>13  && i<=27 ) bandValues[3]  += (int)vReal[i];
       if (i>27  && i<=55 ) bandValues[4]  += (int)vReal[i];
@@ -98,8 +118,12 @@ void loop() {
     // Move peak up
     if (barHeight > peak[band]) {
       peak[band] = barHeight;
-      if (band < 2){
-          beat_detected = true;
+      if (band == 0){
+          beat_detected_low = true;
+      }
+      else if (band >= 7) {
+          //beat_detected_low = true;
+          beat_detected_high = true;
       }
     }
 
@@ -109,20 +133,29 @@ void loop() {
 
   // Decay peak
   EVERY_N_MILLISECONDS(10) {
+    brightness_stripe_0 /= 2;
+    if (brightnness_floodlight > 0) {
+      brightnness_floodlight = 0;
+    }
+    ledcWrite(ledChannel_0, brightness_stripe_0);
+    dacWrite(FLOODLIGHT_OUT, brightnness_floodlight);
+    
     for (byte band = 0; band < NUM_BANDS; band++)
       if (peak[band] > 0) peak[band] -= 1;
   }
 
-  // Blink LED
+  // flash the lights
   EVERY_N_MILLISECONDS(1) {
-      if (beat_detected){
-          digitalWrite(ONBOARD_LED, HIGH);
-          dacWrite(FLOODLIGHT_OUT, 140);
-          beat_detected = false;
+      if (beat_detected_low){
+          brightnness_floodlight = 150;
+          dacWrite(FLOODLIGHT_OUT, brightnness_floodlight);
+          beat_detected_low = false;
+          counter_low++;
       }
-      else {
-          digitalWrite(ONBOARD_LED, LOW);
-          dacWrite(FLOODLIGHT_OUT, 0);
+      if (beat_detected_high){
+          brightness_stripe_0 = 255;
+          ledcWrite(ledChannel_0, brightness_stripe_0);
+          beat_detected_high = false;
       }
   }
 }
